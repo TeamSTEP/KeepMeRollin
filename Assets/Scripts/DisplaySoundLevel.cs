12b3551fe329bdcca05303f1116d1d87fa33cfa0
Hoon Kim <hoonsubin@icloud.com>
2019-09-06 17:21:02 +0900

    added sound level gui

---
+++ Assets/Scripts/DisplaySoundLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DisplaySoundLevel : MonoBehaviour
+{
+    private TextMeshProUGUI displayText;
+
+    public SoundEmitter sourceObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayText = GetComponent<TextMeshProUGUI>();
+        
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        //todo: change this to image rather than just text
+        displayText.text = "Sound Level: " + Mathf.Round(sourceObject.currentSoundLevel);
+        
+    }
+}
